97282715f9158803dfe0806065e0e43ac82fcbd0
PabloRguezz <pablorodirguezz@gmail.com>
2022-04-19 18:30:42 +0100

    Cambiando la ruta del proyecto #2

---
+++ logica/src/main/java/es/iespuerto/logica/Camarero.java
@@ -0,0 +1,10 @@
+package es.iespuerto.logica;
+
+public class Camarero extends Cliente {
+    /**
+     * Metodo para registrar la mesa de la que el camarero realiza el pedido
+     */
+    public void registrarMesaPedido(){
+        
+    }
+}
